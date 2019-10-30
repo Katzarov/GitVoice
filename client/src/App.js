e11f2f8a6582167c4cf120cd8c6917bb68e564f3
Lyoubomir Katzarov <lyoubomir.katzarov@yahoo.com>
2019-10-30 18:15:50 +0100

    cleanup default react app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Test from "./components/test";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
         <Test />
-      </header>
-    </div>
   );
 }
 
