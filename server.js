701ad305b1cae398c7226c7366f43ee931efe786
Lyoubomir Katzarov <lyoubomir.katzarov@yahoo.com>
2019-10-31 16:05:31 +0100

    connect endpoint to github webhook

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+app.use(require('body-parser').json())
 
 app.get('/api/test', (req, res) => {
   const test = 'api works';
-
   res.json(test);
+});
+
+app.post('/api/hook', (req, res) => {
+  console.log(req.body.issue.title)
+  res.json(200)
 });
 
 const port = 5000;
