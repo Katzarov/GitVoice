4974f201370ce75a3555e88d3d7943c64ad0d0f9
Lyoubomir Katzarov <lyoubomir.katzarov@yahoo.com>
2019-11-02 14:53:48 +0100

    set status on succesfull post

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const app = express();
 
-
 // Middleware
 app.use(express.json());
 
-
+// test api
 app.get('/api/test', (req, res) => {
   const test = 'api works';
   res.json(test);
 });
 
+// github webhook test
 app.post('/api/hook', (req, res) => {
   console.log(req.body.issue.title)
-  res.json(200)
+  res.status(200)
 });
 
 const port = 5000;
