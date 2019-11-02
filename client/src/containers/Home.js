672525b95cf7d035283f906661adbb5fd7a1e2ea
Lyoubomir Katzarov <lyoubomir.katzarov@yahoo.com>
2019-11-02 16:36:32 +0100

    add title and subtitle to page

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,17 +1,24 @@
 import React from "react";
-import styles from './Home.module.css'
+import styles from "./Home.module.css";
 //import Main from "../components/Home/Main/Main";
-
+import WaveForm from "../components/Home/WaveForm/WaveForm";
 
 const Home = () => {
   return (
     <div className={styles.Home}>
       <div className={styles.MainSection}>
-
-        <div className={styles.MainButton}>
-          GET STARTED
+        <div className={styles.TitleSubContainer}>
+          <div className={styles.Title}>GIT VOICE</div>
+          <div className={styles.SubTitle}>
+            voice notifications for your github repository
+          </div>
         </div>
 
+        <div className={styles.MainButton}>GET STARTED</div>
+
+        <div className={styles.WaveForm}>
+          <WaveForm />
+        </div>
       </div>
       next section
     </div>
