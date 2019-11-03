1f491bd755382abb2ad536dd15a9cc393b7dc46c
Lyoubomir Katzarov <lyoubomir.katzarov@yahoo.com>
2019-11-03 13:55:16 +0100

    set correct response to git hooks

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/api/test', (req, res) => {
 // github webhook test
 app.post('/api/hook', (req, res) => {
   console.log(req.body.issue.title)
-  res.status(200)
+  res.json(200)
 });
 
 const port = 5000;
