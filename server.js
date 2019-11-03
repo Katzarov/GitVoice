8db4ff6e9955f32c16f46f68667d26ba1dbc2484
Lyoubomir Katzarov <lyoubomir.katzarov@yahoo.com>
2019-11-03 14:25:08 +0100

    connect to DB

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
 
 // Middleware
 app.use(express.json());
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to DB
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(()=> console.log('DB connected'))
+  .catch(err => console.log(err))
 
 // test api
 app.get('/api/test', (req, res) => {
