716a7c629944bd6535e37f8d571122c7f2dc0f2f
Lyoubomir Katzarov <lyoubomir.katzarov@yahoo.com>
2019-11-05 13:56:58 +0100

    fix proptypes

--- client/src/components/Dialog/Dialog.js
+++ client/src/components/Dialog/Dialog.js
@@ -2,28 +2,30 @@ import React, { useState } from "react";
 import styles from "./Dialog.module.css";
 import * as PropTypes from "prop-types";
 
-const Dialog = (
+const Dialog = ({
   open,
   closeCallback,
-  closeButton = "false",
-  backDropClick = "true"
+  closeButton = false,
+  backDropClick = true
+  }
 ) => {
-  const [open, setOpen] = useState(false);
+  //const [open, setOpen] = useState(false);
 
   if (open) {
     return (
-    <div className='styles.BackDrop'>
-    <div className="styles.DialogContainer"></div>
+    <div className={styles.BackDrop}>
+    <div className={styles.DialogContainer}></div>
     </div>
   )
   }
+  else return null
 };
 
 Dialog.propTypes = {
-  open: PropTypes.boolean.isRequired,
-  callback: PropTypes.function,
-  closeButton: propTypes.boolean,
-  backDropClick: propTypes.boolean
+  open: PropTypes.bool.isRequired,
+  callback: PropTypes.func,
+  closeButton: PropTypes.bool,
+  backDropClick: PropTypes.bool
 };
 
 export default Dialog;
